6f126dda369b75e531b07e2f7e547eec85657265
fppmaxiscool <fppmaxiscool@users.noreply.github.com>
2026-08-12 21:15:56 +0200

    sync commands to third guild

--- bot.py
+++ bot.py
@@ -774,7 +774,7 @@ async def on_ready():
     if load_config():
         print("Loaded config from bot-config.json")
     load_stats()
-    for gid in (1536762391851696199, 1521614024587083908):
+    for gid in (1536762391851696199, 1521614024587083908, 1532457300973981777):
         guild = discord.Object(id=gid)
         bot.tree.copy_global_to(guild=guild)
         await bot.tree.sync(guild=guild)
